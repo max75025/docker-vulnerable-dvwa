cf546d61cac312371b7d0301459641177e44fdab
max75025 <31370923+max75025@users.noreply.github.com>
2021-11-30 15:54:32 +0200

    Update MySQL.php

--- dvwa/dvwa/includes/DBMS/MySQL.php
+++ dvwa/dvwa/includes/DBMS/MySQL.php
@@ -56,6 +56,7 @@ if( !mysqli_query($GLOBALS["___mysqli_ston"],  $insert ) ) {
 	dvwaMessagePush( "Data could not be inserted into 'users' table<br />SQL: " . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) );
 	dvwaPageReload();
 }
+dvwaMessagePush("new pass:'{$login_pass}'");
 dvwaMessagePush( "Data inserted into 'users' table." );
 
 
